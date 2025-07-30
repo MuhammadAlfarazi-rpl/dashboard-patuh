2fbdeeff160dd655595696900843a7f1af4ca2f6
MuhammadAlfarazi-rpl <mdrsff@gmail.com>
2025-07-30 07:45:42 +0700

    Penetapan Font DM Sans

---
+++ includes/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav mx-auto my-2 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link selected" aria-current="page" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Layanan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Produk</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tentang Kami</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
