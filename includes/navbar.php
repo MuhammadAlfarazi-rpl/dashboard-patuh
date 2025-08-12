1af7143830a9f39bfbaf3fb1a90cb9f98e648565
MuhammadAlfarazi-rpl <mdrsff@gmail.com>
2025-08-12 10:01:43 +0700

    Navbar Update

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,29 +1,11 @@
-<nav class="navbar bg-body-tertiary my-2 py-2">
-  <div class="container-fluid d-flex justify-content-between align-items-center">
+<nav class="navbar mb-2 py-2">
+  <div class="container-fluid d-flex justify-content-between align-items-center p-2">
 
     <!-- Bagian KIRI -->
     <div class="ms-3 d-flex align-items-center">
       <img src="assets/img/bps.png" alt="BPS" width="30" class="me-2">
       <strong>Badan Pusat Statistik</strong>
     </div>
-
-    <!-- Bagian TENGAH -->
-    <ul class="nav position-absolute start-50 translate-middle-x d-none d-lg-flex">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Beranda</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Layanan</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Informasi</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Produk</a>
-      </li>
-          <span class="underline-indicator"></span>
-    </ul>
-
 
     <!-- Bagian KANAN -->
     <div class="me-3 d-flex align-items-center gap-3">
