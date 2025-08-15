aab1ef1f3787e0ac67f27fb26ba6177531b90efb
TriIntanSyarifah1011 <intansyarifah2023@gmail.com>
2025-08-15 13:58:21 +0700

    Menambahkan halaman pedoman dinas

---
+++ pedoman_dinas.php
@@ -0,0 +1,139 @@
+<?php include 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Pedoman Perjalanan Dinas - BPS KEPRI</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css"> 
+</head>
+<body>
+
+<!-- JUDUL BESAR -->
+<div class="container-fluid mt-5 pt-5">
+  <div class="card shadow-sm">
+    <div class="card-body text-center pb-2 border-bottom border-secondary">
+      <h3 class="fw-bold text-uppercase display-6">
+        Pedoman Pelaksanaan Perjalanan Dinas Jabatan
+      </h3>
+      <h6 class="text-muted">
+        di Lingkungan Badan Pusat Statistik
+      </h6>
+    </div>
+  </div>
+</div>
+
+<!-- MAIN CONTENT: SIDEMENU + KONTEN -->
+<div class="container-fluid mb-5">
+  <div class="card">
+    <div class="row g-0 d-flex align-items-stretch">
+
+      <!-- SIDEMENU -->
+<div class="col-lg-3 sidebar-border-end">
+  <div class="p-3">
+    <?php include 'includes/side_menu.php'; ?>
+  </div>
+</div>
+
+
+      <!-- KONTEN UTAMA -->
+      <div class="col-lg-9">
+        <div class="p-4 tab-content h-100" id="v-pills-tabContent">
+
+          <!-- Ketentuan Umum -->
+        <div class="tab-pane fade show active" id="pills-0" role="tabpanel">
+        <h4>Ketentuan Umum</h4>
+        <p>Dalam Peraturan Kepala Badan Pusat Statistik ini, yang dimaksud dengan:
+          <ol>
+          <li>Surat Perjalanan Dinas yang selanjutnya disebut SPD adalah dokumen yang diterbitkan Pejabat Pembuat Komitmen (PPK) dalam rangka pelaksanaan perjalanan dinas bagi Pejabat Negara, Pegawai Negeri Sipil, Pegawai tidak tetap dan pihak lain.</li>
+          <li>Pelaksana SPD adalah Pejabat Negara, Pegawai Negeri Sipil, Pegawai tidak tetap, dan pihak lain yang melaksanakan perjalanan dinas.</li>
+          <li>Perjalanan Dinas Dalam Negeri yang selanjutnya disebut Perjalanan Dinas adalah perjalanan ke luar Tempat kedudukan yang dilakukan dalam wilayah Republik Indonesia untuk kepentingan negara.</li>
+          <li>Perjalanan Dinas Jabatan adalah Perjalanan Dinas melewati batas Kota dan/atau dalam Kota dari tempat kedudukan ke tempat yang dituju, melaksanakan tugas, dan kembali ke tempat kedudukan semula di dalam negeri.</li>
+          <li>Pejabat Negara adalah pejabat negara sebagaimana dimaksud dalam peraturan perundang-undangan mengenai aparatur sipil negara dan pejabat negara lainnya yang ditentukan oleh Undang-Undang.</li>
+          <li>Pegawai Negeri adalah pegawai negeri sipil sebagaimana dimaksud dalam peraturan perundang-undangan mengenai aparatur sipil negara</li>
+          <li>Pegawai Tidak Tetap adalah pegawai pemerintah dengan perjanjian kerja sebagaimana dimaksud dalam peraturan perundang-undangan mengenai aparatur sipil negara</li>
+          <li>Kuasa Pengguna Anggaran yang selanjutnya disingkat KPA adalah pejabat yang memperoleh kuasa dari Pengguna Anggaran untuk melaksanakan sebagian kewenangan dan tanggung jawab penggunaan anggaran pada Kementerian Negara/Lembaga yang bersangkutan..</li>
+          <li>Pejabat Pembuat Komitmen yang selanjutnya disingkat PPK adalah pejabat yang diberi kewenangan oleh Pengguna Anggaran/Kuasa Pengguna Anggaran untuk mengambil keputusan dan/atau tindakan yang dapat mengakibatkan pengeluaran atas beban Anggaran Pendapatan dan Belanja Negara (APBN).</li>
+          <li>Penanggung jawab kegiatan adalah pejabat eselon II yang bertanggung jawab atas pelaksanaan kegiatan.</li>
+          <li>Ajudan adalah ASN yang diberi tugas tambahan mendampingi pimpinan dalam rangka penyelesaian tugas-tugas protokoler.</li>
+          <li>Lumpsum adalah suatu jumlah uang yang telah dihitung terlebih dahulu (pre-calculated amount) dan dibayarkan sekaligus.</li>
+          <li>Biaya Riil adalah biaya yang dikeluarkan sesuai dengan bukti pengeluaran yang sah.</li>
+          <li>Daftar Pengeluaran Riil adalah rincian biaya yang dikeluarkan yang tidak ada tanda bukti pengeluaran yang sah.</li>
+          <li>Perhitungan rampung adalah perhitungan biaya perjalanan dinas yang dihitung sesuai kebutuhan riil berdasarkan ketentuan yang berlaku.</li>
+          <li>Tempat kedudukan adalah lokasi kantor/satuan kerja.</li>
+          <li>Tempat tujuan adalah tempat/Kota yang menjadi tujuan Perjalanan Dinas.</li>
+          <li>Tempat Sah adalah lokasi Kota Pelaksana SPD berada secara sah, ciantaranya lokasi pelaksanaan fleksibilitas tempat bekerja (flexible working space), lokasi cuti, lokasi ketika sedang menjalani libur resmi, dan Tempat Tujuan penugasan Perjalanan Dinas lainnya</li>
+          <li>Standar Biaya adalah satuan biaya yang ditetapkan sebagai acuan penghitungan kebutuhan anggaran dalam Rencana Kerja dan Anggaran Kementerian Negara/Lembaga, baik berupa Standar Biaya Masukan maupun Standar Biaya Keluaran.</li>
+          <li>APBN adalah Anggaran Pendapatan dan Belanja Negara adalah rencana keuangan tahunan Pemerintah negara Indonesia yang disetujui oleh Dewan Perwakilan Rakyat. APBN berisi daftar sistematis dan terperinci yang memuat rencana penerimaan dan pengeluaran negara selama satu tahun anggaran.</li>
+          </ol>
+        </p>
+        </div>
+
+          <div class="tab-pane fade" id="pills-1" role="tabpanel">
+            <h4>Ruang Lingkup Perjalanan Dinas Jabatan</h4>
+            <p> 
+            <ol>
+              <li>Peraturan Kepala Badan ini mengatur mengenai perencanaan, pelaksanaan dan pertanggungjawaban Perjalanan Dinas Jabatan bagi Pelaksana SPD yang meliputi Pejabat Negara, Pegawai Negeri dan Pegawai Tidak Tetap dan Pihak Lain yang dibebankan pada Anggaran Pendapatan dan Belanja Negara di lingkungan Badan Pusat Statistik.</li>
+              <li>Pegawai Negeri sebagaimana dimaksud pada angka (1) termasuk: 2.
+                <ol type="a">
+              <li>Calon Pegawai Negeri;</li>
+              <li>Prajurit Tentara Nasional Indonesia; dan</li>
+              <li>Anggota Kepolisian Negara Republik Indonesia.</li>
+                </ol>
+            </ol>
+            </p> 
+          </div>
+
+          <div class="tab-pane fade" id="pills-2" role="tabpanel">
+            <h4>Prinsip Perjalanan Dinas Jabatan</h4>
+            <p>
+              <ol>
+                <li>Perjalanan Dinas Jabatan dilaksanakan dengan memperhatikan prinsip.
+                sebagai berikut:</li>
+                <ol type="a">
+                <li>Selektif, yaitu hanya untuk kepentingan yang sangat tinggi dan prioritas yang berkaitan dengan penyelenggaraan pemerintahan;</li>
+                <li>Ketersediaan anggaran dan kesesuaian dengan pencapaian kinerja Kementerian Negara/Lembaga;</li>
+                <li>Efisiensi dan efektivitas dalam penggunaan belanja negara; dan</li>
+                <li>Transparansi dan Akuntabilitas pemberian perintah pelaksanaan Perjalanan Dinas dan pembebanan biaya Perjalanan Dinas Jabatan.</li>
+              </ol>
+            </ol>
+          </p>
+          </div>
+
+          <div class="tab-pane fade" id="pills-3" role="tabpanel">
+            <h4>Perjalanan Dinas Jabatan</h4>
+            <p>Isiiii.</p>
+          </div>
+
+          <div class="tab-pane fade" id="pills-4" role="tabpanel">
+            <h4>Kegiatan Rapat, Konsinyering, dan Sejenisnya</h4>
+            <p>Penjelasan.</p>
+          </div>
+
+          <div class="tab-pane fade" id="pills-5" role="tabpanel">
+            <h4>Biaya Perjalanan Dinas Jabatan</h4>
+            <p>Isi.</p>
+          </div>
+
+          <div class="tab-pane fade" id="pills-6" role="tabpanel">
+            <h4>Pembatalan Perjalanan Dinas</h4>
+            <p>BTAL.</p>
+          </div>
+
+          <div class="tab-pane fade" id="pills-7" role="tabpanel">
+            <h4>Standar Biaya Transportasi</h4>
+            <p>Isi</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+<?php include 'includes/footer.php'; ?>
