a79a5cab241beaff00a62f42c8e1411d3f29d3a8
sharifah-nur <hadiyasharifahnur@gmail.com>
2025-08-25 21:03:27 +0100

    memperbarui sidemenu

--- includes/side_menu2.php
+++ includes/side_menu2.php
@@ -7,8 +7,8 @@
   <a class="nav-link border-bottom py-2" id="tab-5" data-bs-toggle="pill" href="#pills-5" role="tab">Belanja Perjalanan Dinas</a>
   <a class="nav-link border-bottom py-2" id="tab-6" data-bs-toggle="pill" href="#pills-6" role="tab">Belanja Paket Meeting</a>
   <a class="nav-link border-bottom py-2" id="tab-7" data-bs-toggle="pill" href="#pills-7" role="tab">Belanja Sewa</a>
-  <a class="nav-link py-2" id="tab-8" data-bs-toggle="pill" href="#pills-8" role="tab">Belanja Jasa Pengiriman / Ekspedisi</a>
-  <a class="nav-link py-2" id="tab-9" data-bs-toggle="pill" href="#pills-9" role="tab">Belanja Jasa Asuransi</a>
-  <a class="nav-link py-2" id="tab-10" data-bs-toggle="pill" href="#pills-10" role="tab">Belanja Jasa Lainnya</a>
+  <a class="nav-link border-bottom py-2" id="tab-8" data-bs-toggle="pill" href="#pills-8" role="tab">Belanja Jasa Pengiriman / Ekspedisi</a>
+  <a class="nav-link border-bottom py-2" id="tab-9" data-bs-toggle="pill" href="#pills-9" role="tab">Belanja Jasa Asuransi</a>
+  <a class="nav-link border-bottom py-2" id="tab-10" data-bs-toggle="pill" href="#pills-10" role="tab">Belanja Jasa Lainnya</a>
   <a class="nav-link py-2" id="tab-11" data-bs-toggle="pill" href="#pills-11" role="tab">Belanja Pulsa / Paket Data Internet</a>
 </div>
