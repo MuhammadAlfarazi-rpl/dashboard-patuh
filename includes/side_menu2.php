af4f30dac80e68465c475e45c391e7cbc1ac629b
sharifah-nur <hadiyasharifahnur@gmail.com>
2025-08-25 08:40:07 +0100

    menambahkan sidemenu untuk halaman kategori_belanja.php

---
+++ includes/side_menu2.php
@@ -0,0 +1,14 @@
+<div class="nav flex-column nav-pills sidebar-scroll" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+  <a class="nav-link active border-bottom py-2" id="tab-0" data-bs-toggle="pill" href="#pills-0" role="tab">Belanja Konsumsi Rapat</a>
+  <a class="nav-link border-bottom py-2" id="tab-1" data-bs-toggle="pill" href="#pills-1" role="tab">Belanja Perlengkapan Pakai Habis</a>
+  <a class="nav-link border-bottom py-2" id="tab-2" data-bs-toggle="pill" href="#pills-2" role="tab">Belanja Penggandaan / Pencetakan</a>
+  <a class="nav-link border-bottom py-2" id="tab-3" data-bs-toggle="pill" href="#pills-3" role="tab">Belanja Persediaan Barang Konsumsi</a>
+  <a class="nav-link border-bottom py-2" id="tab-4" data-bs-toggle="pill" href="#pills-4" role="tab">Belanja Honorarium</a>
+  <a class="nav-link border-bottom py-2" id="tab-5" data-bs-toggle="pill" href="#pills-5" role="tab">Belanja Perjalanan Dinas</a>
+  <a class="nav-link border-bottom py-2" id="tab-6" data-bs-toggle="pill" href="#pills-6" role="tab">Belanja Paket Meeting</a>
+  <a class="nav-link border-bottom py-2" id="tab-7" data-bs-toggle="pill" href="#pills-7" role="tab">Belanja Sewa</a>
+  <a class="nav-link py-2" id="tab-8" data-bs-toggle="pill" href="#pills-8" role="tab">Belanja Jasa Pengiriman / Ekspedisi</a>
+  <a class="nav-link py-2" id="tab-9" data-bs-toggle="pill" href="#pills-9" role="tab">Belanja Jasa Asuransi</a>
+  <a class="nav-link py-2" id="tab-10" data-bs-toggle="pill" href="#pills-10" role="tab">Belanja Jasa Lainnya</a>
+  <a class="nav-link py-2" id="tab-11" data-bs-toggle="pill" href="#pills-11" role="tab">Belanja Pulsa / Paket Data Internet</a>
+</div>
